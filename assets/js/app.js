9c147f9bcbbae915b6078d1552051b2e1851e653
Slabkiy <slabkiya@gmail.com>
2017-09-19 14:19:16 +0300

    Возможность переименовать блок, удалить

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,59 +2,59 @@ var treeData = {
   "name": "Top Level",
   "id": 1,
   "children": [{
-    "name": "Level 2: A",
+    "name": "Node (level 2)",
     "id": 2,
     "children": [{
-      "name": "Son of A",
+      "name": "Node (level 3)",
       "id": 3,
     }, {
-      "name": "Daughter of A",
+      "name": "Node (level 3)",
       "id": 4,
     }]
   }, {
-    "name": "Level 2: A",
+    "name": "Node (level 2)",
     "id": 5,
     "children": [{
-      "name": "Son of A",
+      "name": "Node (level 3)",
       "id": 6,
     }, {
-      "name": "Daughter of A",
+      "name": "Node (level 3)",
       "id": 7,
     }]
   }, {
-    "name": "Level 2: A",
+    "name": "Node (level 2)",
     "id": 8,
     "children": [{
-      "name": "Son of A",
+      "name": "Node (level 3)",
       "id": 9,
     }, {
-      "name": "Daughter of A",
+      "name": "Node (level 3)",
       "id": 10,
     }]
   }, {
-    "name": "Level 2: A",
+    "name": "Node (level 2)",
     "children": [{
-      "name": "Son of A",
+      "name": "Node (level 3)",
       "id": 11,
     }, {
-      "name": "Daughter of A",
+      "name": "Node (level 3)",
       "id": 12,
     }]
   }, {
-    "name": "Level 2: B",
+    "name": "Node (level 2)",
     "id": 13,
     "children": [{
-      "name": "Level 3: B",
+      "name": "Node (level 3)",
       "id": 14,
       "children": [{
-        "name": "Son of B",
+        "name": "Node (level 4)",
         "id": 15,
       }, {
-        "name": "Daughter of B",
+        "name": "Node (level 4)",
         "id": 16,
       }]
     }, {
-      "name": "Level 3: A",
+      "name": "Node (level 3)",
       "id": 17,
     }]
   }]
@@ -68,12 +68,12 @@ var margin = {
     left: 90
   },
   width = $('.grid').width() - margin.left - margin.right,
-  height = $('.grid').width() - margin.top - margin.bottom;
+  height = $('.grid').height() - margin.top - margin.bottom;
 var rect = {
   width: 200,
   height: 100,
-  rx: 6,
-  ry: 6
+  rx: 3,
+  ry: 3
 };
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
@@ -115,7 +115,7 @@ function collapse(d) {
 }
 
 function update(source) {
-  var newWidth = Math.max(getMaxRow(treemap(root).descendants())*rect.width+margin.left, width);
+  var newWidth = Math.max(getMaxRow(treemap(root).descendants()) * rect.width + margin.left, width);
   d3.select(".grid")
     .attr("width", newWidth + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom);
@@ -151,8 +151,6 @@ function update(source) {
       return "translate(" + source.x0 + "," + source.y0 + ")";
     });
 
-  // Add Circle for the nodes
-
   nodeEnter.append('rect')
     .attr('width', rect.width)
     .attr('height', rect.height)
@@ -161,11 +159,9 @@ function update(source) {
     .attr('rx', rect.rx)
     .attr('ry', rect.ry)
     .style("fill", function(d) {
-      return d._children ? "lightsteelblue" : "#fff";
+      return d._children ? "#64b5f6" : "rgba(255,255,255,1)";
     })
     .on('click', click);
-
-  // Add labels for the nodes
   nodeEnter.append('text')
     .attr("dy", ".35em")
     .attr("x", function(d) {
@@ -178,23 +174,28 @@ function update(source) {
       return d.data.name;
     });
   nodeEnter.append('circle')
-    .attr('class', 'circle add')
-    .attr('r', 10)
+    .attr('class', "circle add")
+    .attr('r', 15)
     .attr('cx', 0)
     .attr('cy', rect.height / 2)
+    .attr('fill', addBackground('add', nodeEnter))
     .on('click', addNewChild);
   nodeEnter.append('circle')
-    .attr('class', 'circle remove')
-    .attr('r', 10)
+    .attr('class', function(d) {
+      return d.depth === 0 ? "circle remove hide" : "circle remove";
+    })
+    .attr('r', 15)
     .attr('cx', rect.width / 2)
     .attr('cy', -rect.height / 2)
+    .attr('fill', addBackground('remove', nodeEnter))
     .on('click', removeNode);
-  /*nodeEnter.append('circle')
+  nodeEnter.append('circle')
     .attr('class', 'circle tools')
-    .attr('r', 10)
+    .attr('r', 15)
     .attr('cx', rect.width / 2)
-    .attr('cy', (-rect.height / 2)+25)
-    .on('click', toolsNode);*/
+    .attr('cy', (-rect.height / 2) + 35)
+    .attr('fill', addBackground('tools', nodeEnter))
+    .on('click', toolsNode);
   // UPDATE
   var nodeUpdate = nodeEnter.merge(node);
 
@@ -206,16 +207,22 @@ function update(source) {
     });
 
   // Update the node attributes and style
-  nodeUpdate.select('rect.node')
+  nodeUpdate.select('rect')
     .attr('class', function(d) {
       return d._children ? "node parent" : "node";
     })
     .style("fill", function(d) {
-      return d._children ? "lightsteelblue" : "#fff";
+      return d._children ? "#64b5f6" : "rgba(255,255,255,1)";
     })
     .attr('cursor', 'pointer');
-
-  // Remove any exiting nodes
+  nodeUpdate.select('text')
+    .text(function(d) {
+      return d.data.name;
+    })
+    .style("fill", function(d) {
+      return d._children ? "#fff" : "#000";
+    })
+    // Remove any exiting nodes
   var nodeExit = node.exit().transition()
     .duration(duration)
     .attr("transform", function(d) {
@@ -224,7 +231,7 @@ function update(source) {
     .remove();
   // On exit reduce the opacity of text labels
   nodeExit.select('text')
-    .style('fill-opacity', 1e-6);
+    .style('fill-opacity', 1e-6)
   nodeExit.select('rect')
     .style('fill-opacity', 1e-6)
     .style('stroke-opacity', 1e-6);
@@ -299,100 +306,202 @@ function update(source) {
     update(d);
   }
 
-  function addNewChild(d) {
-    var newNode = {
-      name: "Новый блок"
-    };
-    //Creates a Node from newNode object using d3.hierarchy(.)
-    var newNode = d3.hierarchy(newNode);
-
-    //later added some properties to Node like child,parent,depth
-    newNode.depth = d.depth + 1;
-    newNode.height = d.height - 1;
-    newNode.parent = d;
-    newNode.id = Date.now();
-
-    //Selected is a node, to which we are adding the new node as a child
-    //If no child array, create an empty array
-    if (d._children) {
-      d._children.push(newNode);
-      Materialize.toast("New children", 2500);
+  function addBackground(type, node) {
+    var id = type + "Circle";
+    if (type === 'add') {
+      var bgCircle = node.append('defs').append('pattern');
+      bgCircle.attr('id', id)
+        .attr('x', '-14.8')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('height', '100%')
+        .attr('width', '150%')
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', 35)
+        .attr('width', '30')
+        .attr('height', '30')
+      bgCircle.append('image')
+        .attr('x', 0)
+        .attr('y', 35)
+        .attr('width', '30')
+        .attr('height', '30')
+        .attr('xlink:href', 'assets/images/' + type + '.svg');
     }
-    if (!d._children) {
-      if (!d.children) {
-        d.children = [];
-        d.data.children = [];
-      }
-      d.children.push(newNode);
-      d.data.children.push(newNode.data);
-      Materialize.toast("New children", 2500);
+    if (type === 'remove') {
+      var bgCircle = node.append('defs').append('pattern');
+      bgCircle.attr('id', id)
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2-15)
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('height', '100%')
+        .attr('width', '150%')
+        .append('rect')
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2-15)
+        .attr('width', '30')
+        .attr('height', '30')
+      bgCircle.append('image')
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2-15)
+        .attr('width', '30')
+        .attr('height', '30')
+        .attr('xlink:href', 'assets/images/' + type + '.svg');
     }
-    if (!d.children) {
-      d.children = d._children;
-      d._children = null;
+    if (type === 'tools') {
+      var bgCircle = node.append('defs').append('pattern');
+      bgCircle.attr('id', id)
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2+20)
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('height', '100%')
+        .attr('width', '150%')
+        .append('rect')
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2+20)
+        .attr('width', '30')
+        .attr('height', '30')
+      bgCircle.append('image')
+        .attr('x', rect.width/2-15)
+        .attr('y', -rect.height/2+20)
+        .attr('width', '30')
+        .attr('height', '30')
+        .attr('xlink:href', 'assets/images/' + type + '.svg');
     }
-    update(d)
-  }
-
-  function removeNode(node) {
-    debug("removeNode\n", "info");
-    debug(node, "info");
-    node.parent.children.forEach(function(item, i, arr) {
-      if (item.id === node.id) {
-        var children = item.children ? item.children : item._children;
-        node.parent.children.splice(i, 1);
-        if (node.parent.children.length === 0) {
-          node.parent.children = null;
-        }
-        if (children) {
-          for (var i = 0; i < children.length; i++) {
-            children[i].parent = node.parent;
-            replaceDepth(children[i]);
-            node.parent.height += children.length;
-            node.parent.children.push(children[i]);
-          }
-        }
-        Materialize.toast("Remove node", 2500);
-        update(node);
-      }
-    });
-
-    function replaceDepth(children) {
-      children.depth -= 1;
-      var ch = children.children ? children.children : children._children;
-      if (ch) {
-        for (var i = 0; i < ch.length; i++) {
-          replaceDepth(ch[i]);
-        }
-      }
-    }
-  }
-  function toolsNode(d){}
-  function getMaxRow(arr){
-    var count = [];
-    for(var i = 0; i < arr.length; i++){
-      if(!count[arr[i].depth]){
-        count[arr[i].depth] = 1;
-      }else{
-        count[arr[i].depth]++;
-      }  
-    }
-    return Math.max.apply(null,count);
+    //Background circle
+    return "url(#" + id + ")";
   }
 }
 
+
+function addNewChild(d) {
+  var newNode = {
+    name: "New node (level " + (d.depth + 1) + ")"
+  };
+  //Creates a Node from newNode object using d3.hierarchy(.)
+  var newNode = d3.hierarchy(newNode);
+
+  //later added some properties to Node like child,parent,depth
+  newNode.depth = d.depth + 1;
+  newNode.height = d.height - 1;
+  newNode.parent = d;
+  newNode.id = Date.now();
+
+  //Selected is a node, to which we are adding the new node as a child
+  //If no child array, create an empty array
+  if (d._children) {
+    d._children.push(newNode);
+    Materialize.toast("New children", 2500);
+  }
+  if (!d._children) {
+    if (!d.children) {
+      d.children = [];
+      d.data.children = [];
+    }
+    d.children.push(newNode);
+    d.data.children.push(newNode.data);
+    Materialize.toast("New children", 2500);
+  }
+  if (!d.children) {
+    d.children = d._children;
+    d._children = null;
+  }
+  update(d)
+}
+
+function removeNode(node) {
+  debug("removeNode\n", "info");
+  node.parent.children.forEach(function(item, i, arr) {
+    if (item.id === node.id) {
+      var children = item.children ? item.children : item._children;
+      node.parent.children.splice(i, 1);
+      if (node.parent.children.length === 0) {
+        node.parent.children = null;
+      }
+      if (children) {
+        for (var i = 0; i < children.length; i++) {
+          children[i].parent = node.parent;
+          replaceDepth(children[i]);
+          node.parent.height += children.length;
+          node.parent.children.push(children[i]);
+        }
+      }
+      Materialize.toast("Remove node", 2500);
+      update(node);
+    }
+  });
+
+  function replaceDepth(children) {
+    children.depth -= 1;
+    var ch = children.children ? children.children : children._children;
+    if (ch) {
+      for (var i = 0; i < ch.length; i++) {
+        replaceDepth(ch[i]);
+      }
+    }
+  }
+}
+
+function toolsNode(d) {
+  $("#nodeTools").modal('open');
+  var scope = angular.element($('body')).scope();
+  scope.$apply(function() {
+    scope.tree.edit(d);
+    Materialize.updateTextFields();
+  });
+  nodeForTools = d;
+}
+
+function getMaxRow(arr) {
+  var count = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (!count[arr[i].depth]) {
+      count[arr[i].depth] = 1;
+    } else {
+      count[arr[i].depth]++;
+    }
+  }
+  return Math.max.apply(null, count) + count[findArrayIndex(count, Math.max.apply(null, count))];
+}
+/**
+ *Find an element in an array
+ * @param  {Array}  array  
+ * @param  {Number} value The required element
+ * @return {Number}       Element index
+ */
+function findArrayIndex(array, value) {
+  if (array.indexOf) {
+    return array.indexOf(value);
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) return i;
+  }
+
+  return -1;
+}
 /**
  * Debug
  * @param  {[String]} msg  [message]
- * @param  {[String]} type [type: "log", "error", "debug", "info"]
+ * @param  {[String]} type [type: "log", "error", "warn", "info"]
  * @return {[Boolead]}     [status]
  */
 function debug(msg, type) {
   console[type](msg);
-  //return true;
 }
-var app = angular.module("appTree", []);
-var treeCtrl = app.controller('treeCtrl', function() {
-  debug("Controller 'treeCtrl' initialized", "info")
 
-});
+function treeCtrl() {
+  debug("Controller 'treeCtrl' initialized", "info");
+  this.node = {
+    data: {
+      name: "Node name"
+    }
+  };
+  this.edit = function(node) {
+    this.node = node;
+  }
+  this.saveChange = function() {
+    update(this.node);
+  }
+}
+var appTree = angular.module("appTree", []);
+appTree.controller('treeCtrl', treeCtrl);
